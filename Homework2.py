d5cddcd20085c257a0df0ebf9a7c340807f97bd8
Advik Sundar <adviksundar@gmail.com>
2024-02-02 00:13:13 -0600

    Did some HW2 Q6(ii)

--- Homework2.py
+++ Homework2.py
@@ -11,3 +11,35 @@ plt.title("BTC-USD,Jan 2018-Jan 2023")
 plt.xlabel("work week")
 plt.ylabel("closing price (USD)")
 plt.show()
+
+# Part (ii)
+
+def moving_average(df, k):
+    if k == 1:
+        return df
+    cols = ["Week", "BTC price"]
+    new_df = pd.DataFrame(columns=cols)
+    new_value = 0
+    for i in range(k//2, df.shape[0] - k//2):
+        for j in range(i-k//2, (i+k//2) + 1):
+            new_value += df.iloc[j, 'BTC price']
+        new_row = {'Week': i, 'BTC price': new_value}
+        new_df = new_df.append(new_row, ignore_index=True)
+        new_value = 0
+    return new_df
+
+
+y_7_week = moving_average(df, 7)
+y_15_week = moving_average(df, 15)
+
+plt.plot(y_7_week["Week"], y_7_week["BTC price"])
+plt.title("k = 7")
+plt.xlabel("work week")
+plt.ylabel("closing price (USD)")
+plt.show()
+
+plt.plot(y_15_week["Week"], y_15_week["BTC price"])
+plt.title("k = 7")
+plt.xlabel("work week")
+plt.ylabel("closing price (USD)")
+plt.show()
