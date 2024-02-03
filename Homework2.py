36bc7d8046a6e25ccbb87aded12593995e6f9882
Advik Sundar <adviksundar@gmail.com>
2024-02-02 19:04:47 -0600

    Finished HW2

--- Homework2.py
+++ Homework2.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 csv_file = "HW2_BTC.csv"
 df = pd.read_csv(csv_file)
 
-plt.plot(df["Week"], df["BTC price"])
+plt.figure(1)
+plt.plot(df["Week"], df["BTC price"], label='Part(i)')
 plt.title("BTC-USD,Jan 2018-Jan 2023")
 plt.xlabel("work week")
 plt.ylabel("closing price (USD)")
-plt.show()
 
 # Part (ii)
 
@@ -22,9 +22,10 @@ def moving_average(df, k):
     new_value = 0
     for i in range(k//2, df.shape[0] - k//2):
         for j in range(i-k//2, (i+k//2) + 1):
-            new_value += df.iloc[j, 'BTC price']
-        new_row = {'Week': i, 'BTC price': new_value}
-        new_df = new_df.append(new_row, ignore_index=True)
+            new_value += df.iloc[j, 1]
+        new_value /= k
+        new_row = {"Week": i, "BTC price": new_value}
+        new_df.loc[len(new_df)] = new_row
         new_value = 0
     return new_df
 
@@ -32,14 +33,8 @@ def moving_average(df, k):
 mavg7 = moving_average(df, 7)
 mavg15 = moving_average(df, 15)
 
-plt.plot(mavg7["Week"], mavg7["BTC price"])
-plt.title("k = 7")
-plt.xlabel("work week")
-plt.ylabel("closing price (USD)")
-plt.show()
+plt.plot(mavg7["Week"], mavg7["BTC price"], label='k=7 - part(ii)')
+plt.plot(mavg15["Week"], mavg15["BTC price"], label='k=15 - part(ii)')
 
-plt.plot(mavg15["Week"], mavg15["BTC price"])
-plt.title("k = 15")
-plt.xlabel("work week")
-plt.ylabel("closing price (USD)")
+plt.legend()
 plt.show()
